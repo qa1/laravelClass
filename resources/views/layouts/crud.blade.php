4422a0f64803c07daf4f0188f00994422c92844f
Hossein Yousefzade <hossein.yousefzade@gmail.com>
2021-10-20 09:51:22 +0330

    change template

--- resources/views/layouts/crud.blade.php
+++ resources/views/layouts/crud.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 8 CRUD Application - ItSolutionStuff.com</title>
+    <title>Laravel 8 CRUD Application</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
