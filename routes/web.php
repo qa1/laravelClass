77e7c23d371430b7dad8af0284a8b2f07ce41316
Hossein Yousefzade <hossein.yousefzade@gmail.com>
2021-10-20 09:55:19 +0330

    add test route: test route is a simple route for show how routes work

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function () {
+    echo 'There is a test here!';
+});
 
 Route::get('/', function () {
     return view('welcome');
