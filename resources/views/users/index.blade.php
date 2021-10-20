d8fff9447851c4d431d957eed3fb5a56e03328fc
Hossein Yousefzade <hossein.yousefzade@gmail.com>
2021-10-20 10:06:31 +0330

    remove unnessaccery explain on views

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD Example from scratch - ItSolutionStuff.com</h2>
+                <h2>Laravel 8 CRUD Example from scratch</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('users.create') }}"> Create New user</a>
